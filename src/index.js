7d688675facdb50212ede8ac55e35865471754bf
Romério Lima de Sousa <romerio.unix@gmail.com>
2019-10-07 18:16:46 -0300

    Created method interface

--- src/index.js
+++ src/index.js
@@ -1,3 +1,54 @@
-exports.printMsg = function() {
-    console.log("This is a message from the demo package");
+const { IgApiClient } = require('instagram-private-api');
+const ig = new IgApiClient();
+
+exports.loginUser = async ({ username, password }) => {
+    try {
+        ig.state.generateDevice(process.env.IG_USERNAME || username);
+        await ig.simulate.preLoginFlow();
+
+        const userClient = await ig.account.login(process.env.IG_USERNAME || username, process.env.IG_PASSWORD || password);
+
+        return userClient
+    } catch (error) {
+        throw error
+    }
+}
+
+exports.getChatWithNewMessages = async ({ userClient, lastMessageTimestamp }) => {
+    try {
+        
+    } catch (error) {
+        throw error
+    }
+}
+
+/**
+ * Search all chats with new messages
+ * and, for each chat, fetch all new messages
+ */
+
+exports.getAllNewChatMessages = async ({ userClient, lastMessageTimestamp }) => {
+    try {
+        
+    } catch (error) {
+        throw error
+    }
+}
+
+exports.sendNewChatMessage = async ({ userClient, message }) => {
+    try {
+        const type = message.type
+        const content = message.content
+
+    } catch (error) {
+        throw error
+    }
+}
+
+exports.approveAllNewChats = async ({ userClient }) => {
+    try {
+
+    } catch (error) {
+        throw error
+    }
 }
