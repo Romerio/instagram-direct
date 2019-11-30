f6af56ca1463ec702c7d9a074cd6f6dd5cf9b9ce
Romerio <romerio.unix@gmail.com>
2019-11-30 20:19:14 -0300

    Handle de erros

--- examples/receiveDirectMessage.js
+++ examples/receiveDirectMessage.js
@@ -8,13 +8,13 @@ const username2 = 'kinbox.koruja@gmail.com'
 
 const password = 'krjknbx84902'
 
-const execTest = async () => {
+const receiveMessages = async () => {
     try {
-        ig.state.generateDevice(username2);
+        ig.state.generateDevice(username);
         ig.state.proxyUrl = process.env.IG_PROXY;
 
         await ig.simulate.preLoginFlow();
-        await ig.account.login(username2, password);
+        await ig.account.login(username, password);
 
         process.nextTick(async () => await ig.simulate.postLoginFlow());
 
@@ -29,8 +29,35 @@ const execTest = async () => {
         console.log('- Mensagens:', threads[0].items.length)
         console.log(threads[0].items) // Mensagens da primeira conversa da lista
     } catch (error) {
-        console.log('- error')
-        console.log(error)
+        /*console.log('- error')
+        console.log(Object.keys(error))
+        console.log(error)*/
+
+        throw error
+    }
+}
+
+const handleErros = ({ error }) => {
+    try {
+        const errorString = error.toString()
+
+        if (errorString.includes('challenge_required')) {
+            throw new Error('Por favor, utilize o código de autenticação para logar com sua conta.')
+        }
+
+        throw error
+    } catch (e) {
+        console.log('ERROR:')
+        console.log(Object.keys(e))
+        console.log(e)
+    }
+}
+
+const execTest = async () => {
+    try {
+        const result = await receiveMessages()
+    } catch (error) {
+        handleErros({ error })
     }
 }
 
